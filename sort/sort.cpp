57befca6e0928d05143814cf7078c7e633030f3d
foocoder <slow295185031@gmail.com>
2016-07-19 20:36:08 +0800

    add bubble selection insertion shell sort

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -13,39 +13,83 @@
 
 using namespace std;
 
-class Solution{
-    vector<int> sequence;
+class MySort{
     public:
-    Solution():sequence({8,34,3,13,1,21,5,2}){};
-    void BubbleSort(){
-        int sizeOfSeq = sequence.size();
-        for(int ix=0; ix<sizeOfSeq; ix++){
-            for(int jx=ix+1; jx<sizeOfSeq; jx++){
-                if(sequence[ix] > sequence[jx]){
-                    Swap(sequence[ix], sequence[jx]);
-                }
+        template<class T>
+        void
+        BubbleSort( T & num, int size ){
+            bool flag;
+            for(int i=0; i<size && !flag; ++i) {
+                flag = true;
+                for(int j=0; j<size-i; ++j)
+                    if( num[j]>num[j+1] ) {
+                        Swap( num[j], num[j+1] );
+                        flag = false;
+                    }
             }
         }
-    }
-    void Swap(int & a, int & b){
-        int tmp = a;
-        a = b;
-        b = tmp;
-    }
-    void Display(){
-        for(auto i:sequence)
-            cout<<i<<",";
-        cout<<"\b "<<endl;
-    }
+
+        template<class T>
+        void
+        SelectionSort( T & num, int size ){
+            int min;
+            for(int i=0; i<size ; ++i) {
+                min = i;
+                for(int j=i; j<size; ++j)
+                    if( num[min] > num[j])
+                        min = j;
+                Swap( num[min], num[i] );
+            }
+        }
+
+        template<class T>
+        void
+        InsertionSort( T & num, int size ){
+            for( int i=1; i<size; ++i )
+                for( int j=i; j>=1 && num[j] < num[j-1]; --j )
+                    Swap( num[j], num[j-1] );
+        }
+
+        template<class T>
+        void
+        ShellSort( T & num, int size ){
+            int h=1;
+            while( h < size/3 )
+                h = 3*h + 1;
+            while( h >= 1 ){
+                for( int i=h; i<size; ++i ){
+                    for( int j=i; j>=h && num[j] < num[j-h]; j-=h )
+                        Swap( num[j], num[j-h] );
+                }
+                h = h/3;
+            }
+        }
+
+        template<class T>
+        void
+        Swap(T & a, T & b){
+            if( &a == &b )
+                return;
+            a = a^b;
+            b = a^b;
+            a = a^b;
+        }
+
+        template<class T>
+        void
+        Display( T & num ){
+            for( auto i:num )
+                cout<<i<<" ";
+            cout<<endl;
+        }
 };
 
 int main(int argc, char *argv[])
 {
-    Solution test;
-    cout<<"Original Sequence:";
-    test.Display();
-    test.BubbleSort();
-    cout<<" Sorted Sequence :";
-    test.Display();
+    vector<int> seq = { 8, 2,4,1,5,7,3,6,9,0 };
+    MySort test;
+    test.Display( seq );
+    test.ShellSort( seq, seq.size() );
+    test.Display( seq );
     return 0;
 }
